5f2ef996c1b24f7d683e5f2d23fe84c6539a1447
Michael Rich <mtrich@satx.rr.com>
2019-02-04 02:19:44 -0600

    Created Project added Models and relationships and migrated to sqlserver

---
+++ VolunterSite.WebUI/VolunteerSite.Domain/Models/GroupMember.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VolunteerSite.Domain.Models
+{
+    public class GroupMember
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public int TotalHours { get; set; }
+
+        public string VolunteerGroupId { get; set; }
+        public VolunteerGroup VolunteerGroup { get; set; }
+    }
+}
